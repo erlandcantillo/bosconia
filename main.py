3788b305c3bbd4e0b5e164d48bfa7b846d12feee
erland <edcm06@gmail.com>
2024-06-26 20:08:32 -0500

    actualizacion

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-#esto es un ejemplo
-#estas asustado?
-
-from fastapi import FastAPI, HTTPException, Depends
-from sqlalchemy.orm import Session
-from sqlalchemy import text
-from typing import List
-import models, schemas
 from database import SessionLocal, engine, test_connection
-
-models.Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
-
 
 # Dependencia
 def get_db():
@@ -20,79 +7,6 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-@app.post("/libros/", response_model=schemas.Libro)
-async def create_libro(libro: schemas.LibroCreate, db: Session = Depends(get_db)):
-    db_libro = models.Libro(autor=libro.autor, titulo=libro.titulo, estado=libro.estado)
-    db.add(db_libro)
-    db.commit()
-    db.refresh(db_libro)
-    return db_libro
-
-
-@app.get("/libros/", response_model=List[schemas.Libro])
-async def read_libros(db: Session = Depends(get_db)):
-    return db.query(models.Libro).all()
-
-
-@app.post("/usuarios/", response_model=schemas.Usuario)
-async def create_usuario(usuario: schemas.UsuarioCreate, db: Session = Depends(get_db)):
-    db_usuario = models.Usuario(nombre=usuario.nombre, documento=usuario.documento)
-    db.add(db_usuario)
-    db.commit()
-    db.refresh(db_usuario)
-    return db_usuario
-
-
-@app.get("/usuarios/", response_model=List[schemas.Usuario])
-async def read_usuarios(db: Session = Depends(get_db)):
-    return db.query(models.Usuario).all()
-
-
-@app.post("/libros_usuarios/", response_model=schemas.LibroUsuario)
-async def create_libro_usuario(libro_usuario: schemas.LibroUsuarioCreate, db: Session = Depends(get_db)):
-    db_libro_usuario = models.LibroUsuario(
-        usuario_id=libro_usuario.usuario_id,
-        libro_id=libro_usuario.usuario_id,
-        tiempo_prestamo=libro_usuario.tiempo_prestamo
-    )
-    db.add(db_libro_usuario)
-    db.commit()
-    db.refresh(db_libro_usuario)
-    return db_libro_usuario
-
-
-@app.get("/libro/{libro_id}", response_model=schemas.Libro)
-async def read_libro(libro_id: int, db: Session = Depends(get_db)):
-    db_libro = db.query(models.Libro).filter(models.Libro.id == libro_id).first()
-    return db_libro
-
-
-@app.get("/libros/search/", response_model=List[schemas.Libro])
-async def search_libros(query: str, db: Session = Depends(get_db)):
-    db_libro = db.query(models.Libro).filter(models.Libro.titulo.like(f"%{query}%")).first()
-    return db_libro
-
-
-@app.put("/libro/{libro_id}", response_model=schemas.Libro)
-async def update_libro(libro_id: int, libro: schemas.LibroCreate, db: Session = Depends(get_db)):
-    db_libro = db.query(models.Libro).filter(models.Libro.id == libro_id).first()
-    db_libro.autor = libro.autor
-    db_libro.titulo = libro.titulo
-    db_libro.estado = libro.estado
-    db.commit()
-    db.refresh(db_libro)
-    return db_libro
-
-
-@app.delete("/libro/{libro_id}", response_model=dict)
-async def delete_libro(libro_id: int, db: Session = Depends(get_db)):
-    db_libro = db.query(models.Libro).filter(models.Libro.id == libro_id).first()
-    db.delete(db_libro)
-    db.commit()
-    return {"message": "Libro eliminado"}
-
 
 if _name_ == "_main_":
     import uvicorn
